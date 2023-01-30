accc5b212581c06c4f711035156c506037292c67
MrVictor42 <guin-hu@hotmail.com>
2023-01-30 00:53:22 -0300

    Finishing template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: 'app.component.html', 
-    
-    // <router-outlet></router-outlet>,
+	styleUrls: ['./app.component.css'],
 	styles: []
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+	
+	ngAfterViewInit(): void {
+		(function($) {
+			"use strict";
+		
+			var path = window.location.href; 
+				$("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+					if (this instanceof HTMLAnchorElement && this.href === path) {
+						$(this).addClass("active");
+					}
+				});
+		
+			$("#sidebarToggle").on("click", function(e) {
+				e.preventDefault();
+				$("body").toggleClass("sb-sidenav-toggled");
+			});
+		})(jQuery);
+	}
 	title = 'sea-todo-list';
 }
